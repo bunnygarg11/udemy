32029627512961f022466dfd9f36cf6e643c8aa8
mohitgarg <mohitgarg083@gmail.com>
2019-11-01 15:55:20 +0530

    section 11 index 87

--- task-manager/src/mongoose.js
+++ task-manager/src/mongoose.js
@@ -1,21 +1,46 @@
 const mongoose=require("mongoose")
+const validator=require("validator")
 
 mongoose.connect("mongodb://localhost:27017/task-manager-api",{
+    useUnifiedTopology:true,
     useNewUrlParser:true,
 useCreateIndex:true})
 
 const user=mongoose.model("user",{
     name:{
-        type:String
-    },
+        type:String,
+        required:true,
+        trim:true
+        }
+    ,
 
     age:{
-        type:Number
+        type:Number,
+        default:0,
+        validate(value){
+            if(value<0){
+                throw new Error("must be positive")
+            }
+
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw Error("enter valid email")
+            }
+
     }
+}
 })
 const me=new user({
-    name:"bunny",
-    age:22
+    name:"bunny ",
+    age:22,
+    email:"MOHITGARG083@GMAIL.COM"
 })
 me.save().then(()=>{
     console.log(me)
